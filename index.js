dc30063ad636ade2157104743c5d370e1e44c3bf
PArns <github@patrick-arns.de>
2017-01-16 16:46:58 +0100

    initial release

---
+++ index.js
@@ -0,0 +1,34 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var fs = require('fs');
+var bodyParser = require('body-parser');
+
+server.listen(8080);
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+    fs.readFile(__dirname + '/htdocs/index.html',
+        function (err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading index.html');
+            }
+
+            res.writeHead(200);
+            res.end(data);
+        });
+});
+
+app.all('/netatmo-webhook/', function (req, res) {
+    var requestData = req.body;
+
+    if (requestData && requestData.credentials && requestData.credentials.user && requestData.credentials.user.$id) {
+        var id = requestData.credentials.user.$id;
+
+        io.sockets.in(id).emit('alert', requestData);
+    }
+
+    res.writeHead(200);
+    res.end("OK");
+});
